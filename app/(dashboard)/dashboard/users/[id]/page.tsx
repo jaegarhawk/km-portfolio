22b5e7281208f4dacccc163a7b791a5da20dae44
Kendal Mitchell <kmat4u@hotmail.com>
2026-08-31 16:43:46 -0400

    Update page.tsx

--- app/(dashboard)/dashboard/users/[id]/page.tsx
+++ app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
+export async function generateStaticParams() {
+    return [
+        { id: '1' },
+        { id: '2' },
+        { id: '3' },
+        { id: '4' },
+    ]
+}
+
 const UserDetails = async ({ params}: { params: Promise<{ id: string}>}) => {
     const { id } = await params;
     return (
